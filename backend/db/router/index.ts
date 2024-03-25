a7162864e51d835c6fa9b93ee539f1fbe8fd9aa7
Thomas Brace <thomas.brace@titanacademy.io>
2024-03-25 09:28:10 +0000

    Add get groups

--- backend/db/router/index.ts
+++ backend/db/router/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "./types";
 import { buildUserRoutes } from "./users";
 import { buildTeamsRoutes } from "./teams";
+import { buildGroupsRoutes } from "./groups";
 
 export const buildApiRoutes: Router = (fastify, _, done) => {
   fastify.register(buildUserRoutes, { prefix: "/users" });
   fastify.register(buildTeamsRoutes, { prefix: "/teams" });
+  fastify.register(buildGroupsRoutes, { prefix: "/groups" });
   done();
 };
