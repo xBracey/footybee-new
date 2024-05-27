48d2372386f181be6a3122532176c45de4d70fd9
Thomas Brace <thomas.brace@titanacademy.io>
2024-05-27 10:25:13 +0100

    Add pull to refresh

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,21 +1,31 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import PullToRefresh from "react-simple-pull-to-refresh";
 
 export const Route = createRootRoute({
   component: () => (
-    <div
-      className="bg-shamrock-500 relative overflow-auto"
-      style={{ minHeight: "100dvh" }}
+    <PullToRefresh
+      onRefresh={() =>
+        new Promise(() => {
+          window.location.reload();
+        })
+      }
+      backgroundColor="#0C5941"
+      className="text-white"
     >
-      <Header />
       <div
-        className={`absolute inset-0 bottom-14 top-14 overflow-auto p-2 pb-[calc(env(safe-area-inset-bottom,1rem)+0.5rem)] md:bottom-12 md:top-24 md:p-4 md:pb-6`}
+        className="bg-shamrock-500 relative overflow-auto text-gray-900"
+        style={{ minHeight: "100dvh" }}
       >
-        <Outlet />
+        <Header />
+        <div
+          className={`absolute inset-0 bottom-14 top-14 overflow-auto p-2 pb-[calc(env(safe-area-inset-bottom,1rem)+0.5rem)] md:bottom-12 md:top-24 md:p-4 md:pb-6`}
+        >
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </PullToRefresh>
   ),
 });
